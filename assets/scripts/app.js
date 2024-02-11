9760154a193b2f13695000cd1a420e7c6cd20c2d
chetana.lotliker <chetanalotliker@gmail.com>
2024-02-11 11:11:17 +0530

    Update the folder structure and learn export, import

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+import { apiKey } from "./util.js";
+
+// Export using default
+import apiKey from "./util.js";
+import { apiKey, abc as content } from "./util.js";
+
+import * as util from "./util.js";
+
---
+++ assets/scripts/util.js
@@ -0,0 +1,6 @@
+// Export using defaut 
+export default "mock-sample-key";
+
+export let apiKey = "mock-sample-key";
+
+export let abc = "abc";
